a6a54352bab2726cb07d1d2f83ac19a059413d1e
Oleg Yakovlev <yaoleg96@gmail.com>
2023-02-11 19:39:17 +0300

    Gradle kts publish ready

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Convert number systems from one to another.
 
 Invoke `NumSys.convert(value = NumberSystem(), toRadix = Radix())` to get result with required radix.
+
+[![](https://jitpack.io/v/jedi1150/numsys.svg)](https://jitpack.io/#jedi1150/numsys)
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,14 +1,33 @@
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.8.0"
     `java-library`
+    `maven-publish`
 }
 
 repositories {
     mavenCentral()
 }
 
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+publishing {
+    publications {
+        register<MavenPublication>("mavenJava") {
+            groupId = "com.github.jedi1150"
+            artifactId = "numsys"
+            version = "0.0.1"
+
+            afterEvaluate {
+                from(components["java"])
+            }
+        }
+    }
 }
