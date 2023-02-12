3dd2d9a5e322fb44e9962e2ec7fae17cd62aee1b
Oleg Yakovlev <yaoleg96@gmail.com>
2023-02-13 01:40:55 +0300

    version updated to 0.0.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
         register<MavenPublication>("mavenJava") {
             groupId = "com.github.jedi1150"
             artifactId = "numsys"
-            version = "0.0.1"
+            version = "0.0.2"
 
             afterEvaluate {
                 from(components["java"])
